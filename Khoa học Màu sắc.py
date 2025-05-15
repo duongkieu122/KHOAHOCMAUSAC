df0d82277a7273b04fa0f989f6e91e2f3868da60
duongkieu122 <09112003duong@gmail.com>
2025-05-15 12:17:13 +0700

    Update Khoa học Màu sắc.py

--- Khoa học Màu sắc.py
+++ Khoa học Màu sắc.py
@@ -3,5 +3,20 @@ st.set_page_config(
     page_title="Khoa học Màu sắc", layout="wide"
 )
 
-st.write("### Chào mừng bạn đến với project Khoa học Màu sắc của mình!")
-st.write("# Mình tên là Võ Tự Thanh, MaSV 22158139")
+col1, col2 = st.columns([4, 1])
+with col1:
+    st.markdown("<div class='title'>Chào mừng bạn đến với project <i>Khoa học Màu sắc</i>!</div>", unsafe_allow_html=True)
+    st.markdown("<div class='subtitle'>Dự án này được thực hiện bởi nhóm sinh viên HCMUTE.</div>", unsafe_allow_html=True)
+    
+    st.markdown("""
+    <div class='info-box'>
+        <h4>Thành viên nhóm:</h4>
+        <ul>
+            <li><b>Kiều Hồng Dương</b> - MSSV: 22158050</li>
+            <li><b>Tống Ngân Huỳnh</b> - MSSV: 22158061</li>
+        </ul>
+    </div>
+    """, unsafe_allow_html=True)
+
+with col2:
+    st.image("logo-hcmute.jpg", width=140)
